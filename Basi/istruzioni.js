ce48191dee7a7cbb830aa292943c914f7ee11d87
antoStr <antostr.dev@gmail.com>
2025-03-15 17:01:42 +0100

    first commit

---
+++ Basi/istruzioni.js
@@ -0,0 +1,24 @@
+// Esempi di istruzioni attive in Javascript e vengono considerate attive, quindi possono essere eseguite come istruzioni come per esempio assegnazione di una variabile, utilizzo di un metodo in un array o di una stringa etc.
+
+let x = 1;
+const y = 1;
+
+"ciao".toUpperCase();
+[10, 20, 30].push[40];
+
+if (x == 1) {
+  x += 1;
+  x = 100;
+}
+
+console.log(x);
+
+// Tutto ciò che è messo nei commenti viene considerato inattivo e quindi ignorato dal programma, vengono utilizzati nel debug o commentare e documentare
+
+// Commento a linea singola
+
+/* 
+  Commento
+  multi 
+  linea
+*/
