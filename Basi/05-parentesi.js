4293e32e06e54e7b4c64c8843667708a22ed588c
antoStr <antostr.dev@gmail.com>
2025-03-28 16:42:23 +0100

    hoisting

---
+++ Basi/05-parentesi.js
@@ -0,0 +1,11 @@
+/* Le parentesi graffe vengono utilizzate per definire dei blocchi di codice o per la produzione di letterali oggetti
+
+Nella produzione di cicli con una sola istruzione le graffe possono essere omesse 
+
+*/
+
+x = 10;
+
+if (x == 10) {
+  console.log("x ha valore 10");
+}
